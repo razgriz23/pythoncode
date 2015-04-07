91b86f9afc399d1fbf063cfaeca9cb9f46a94be2
Rob <rbniesen@gmail.com>
2015-04-07 13:13:37 -0500

    update

--- em.py
+++ em.py
@@ -4,78 +4,121 @@
 import collections,math,sys
 from pprint import pprint
 
-
-data = (72, 61.0, 69.0, 74.0, 69.0, 68.0, 74.0, 68.0, 61.0, 61.0, 72, 65.0, 73.0, 63.0, 62.0, 64.0, 60, 64.0, 69.0, 62.0, 64.0, 62.0);
-std = 3
-datalistEStepA = []
-datalistEStepB = []
-mstepA = []
-mstepB = []
-k = []
+data = (72, 61.0, 69.0, 74.0, 69.0, 68.0, 74.0, 68.0, 61.0, 61.0, 72, 65.0, 73.0, 63.0, 62.0, 64.0, 60, 64.0, 69.0, 62.0, 64.0, 62.0)
+newmeans = []
 
 
 
-def norm_density(datalist, sigma, mean1, mean2):  #E step calculation
-	listmeanA = []
-	listmeanB = []
+
+def norm_density(datalist, means, k, sigma):  #E step calculation
+	denom = []
 	calcDenom = []
-	for index in range(len(datalist)): #calculate the numerator for both sets
-		listmeanA.append(math.exp((-1/(2*math.pow(sigma,2))) * math.pow((datalist[index] - mean1), 2)))
-		listmeanB.append(math.exp((-1/(2*math.pow(sigma,2))) * math.pow((datalist[index] - mean2), 2)))
-		calcDenom = listmeanA[index] + listmeanB[index] #calculate the denominator
-		datalistEStepA.append(listmeanA[index] / calcDenom) #final results in a list
-		datalistEStepB.append(listmeanB[index] / calcDenom)
-	return datalistEStepA, datalistEStepB
+	numerator = []
+	a = 0
+	x = 0
+	z = 0
+	while x < len(means):
+		for index in range(len(datalist)): #calculate the numerator
+			numerator.append(math.exp((-1/(2*math.pow(sigma,2))) * math.pow((datalist[index] - means[x]), 2)))
+		x += 1
+	while z < len(means):
+		size = len(numerator)
+		r = 0
+		n = 0
+		p = 0
+		while r < len(numerator):
+			a += numerator[r]
+			z += 1
+			r += len(datalist)
+			p += 1
+			if p == len(means):
+				calcDenom.append(a)
+				n += 1
+				r = n
+				a = 0
+				p = 0
+						
+	#pprint(calcDenom)
+	
+	divided = []
+	dividedsum = []
+	dividedsums = []
 
-def mStep(datalist): #M step calculation
-	for index in range(len(datalistEStepA)):
-		mstepA.append(datalistEStepA[index] * datalist[index])
-		mstepB.append(datalistEStepB[index] * datalist[index])
-	mstepAsum = sum(mstepA)
-	mstepBsum = sum(mstepB)
-	sumEstepA = math.fsum(datalistEStepA)
-	sumEstepB = math.fsum(datalistEStepB)
-	mStepACalc = mstepAsum / sumEstepA
-	mStepBCalc = mstepBsum / sumEstepB
+	x = 0
+	for index in range(len(numerator)):
+		divided.append(numerator[index] / calcDenom[x])
+		dividedsum.append(numerator[index] / calcDenom[x])
+		x += 1
+		if x == len(calcDenom):
+			x = 0
+			f = sum(dividedsum)
+			del dividedsum[:]
+			dividedsums.append(f)
+			f = 0
+	
+	#pprint(divided)
+	#pprint(dividedsums)
 
-	global newmeanA, newmeanB
-	newmeanA = mStepACalc
-	newmeanB = mStepBCalc
-	del datalistEStepA[:] #clear the list to start over with the new means
-	del datalistEStepB[:]
-	del mstepA[:]
-	del mstepB[:]
-	return newmeanA, newmeanB
+	multiplied = []
+	multipliedsums = []
+	multipliedsumsfornewmeans = []
+
+	x = 0
+	for index in range(len(divided)):
+		multiplied.append(divided[index] * datalist[x])
+		multipliedsums.append(divided[index] * datalist[x])
+		x += 1
+		if x == len(datalist):
+			x = 0
+			f = sum(multipliedsums)
+			del multipliedsums[:]
+			multipliedsumsfornewmeans.append(f)
+			f = 0
+
+	#pprint(multiplied)
+	#pprint(multipliedsumsfornewmeans)
+
+	for index in range(len(multipliedsumsfornewmeans)):
+		newmeans.append(multipliedsumsfornewmeans[index] / dividedsums[index])
+
+	pprint(newmeans)
+	#pprint(datalistEStep)
+
+
+	return newmeans
+
+
 
 	
 def em(data, k, std): #calls our functions above to get our means that result from convergence
-	meanA = data[0]
-	meanB = data[1]
-	norm_density(data, std, meanA, meanB)
-	mStep(data)
+	means = []
+	for x in range(0,k):
+		means.append(data[x])
+		x += 1
 
-	meanDiffA = newmeanA - meanA
-	meanDiffB = newmeanB - meanB
-	if (meanDiffA < 0):
-		meanDiffA *= -1 #some results end up being negative. Need to ensure this doesn't happen
+	norm_density(data, means, k, std)
+
+	newmean = 0
+	meanDiff = newmean - newmeans[0]
+	if meanDiff < 0:
+		meanDiff = meanDiff * -1
+		
 	x = 0
+	means = list(newmeans)
+	del newmeans[:]
+	while (meanDiff > .0001): #keep calculating our sets with the new means until we find convergence	
+		norm_density(data, means, k, std)
 
-	while (meanDiffA > .0001): #keep calculating our sets with the new means until we find convergence
-		norm_density(data, std, newmeanA, newmeanB)
-		meanA = newmeanA
-		meanB = newmeanB
-
-		mStep(data)
-
-		meanDiffA = newmeanA - meanA
-		meanDiffB = newmeanB - meanB
-	
-		if (meanDiffA < 0):
-			meanDiffA *= -1
+		meanDiff = means[0] - newmeans[0]
+		if meanDiff < 0:
+			meanDiff = meanDiff * -1
+		del means[:]
+		means = list(newmeans)
+		del newmeans[:]
 
 		x += 1
-	k = [meanA, meanB]
-	pprint(k)
+	print means
 	print "Number of iterations: ", x
+	
 
-em(data, k, std)
+em(data, 2, 3)
